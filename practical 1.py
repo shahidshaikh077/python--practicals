36a5b632e6df511740d6270501527c17843192ed
shahidshaikh077 <shahidahamadshaikh07@gmail.com>
2026-09-12 16:03:35 +0530

    Add files via upload

---
+++ practical 1.py
@@ -0,0 +1,8 @@
+print("======STUDENT DETAILS======")
+name=str(input("Enter your name:"))
+age=int(input("Enter your age:"))
+roll_no=int(input("Enter your roll no:")) 
+print("=====STUDENT DETAILS===")
+print("Name is:",name)
+print("Age is:",age)
+print("Roll NO is:",roll_no) 
